5a2cb15c4ebfdbd966bb5c8aa42f038d72839949
badmashagaya <waliasamarth2020@gmail.com>
2026-04-01 10:43:41 +0530

    Update server.js

--- server.js
+++ server.js
@@ -6,7 +6,7 @@ const app = express();
 
 // --- Configuration (Set these in Render's Environment Variables) ---
 const PORT = process.env.PORT || 3000;
-const PYTHON_API_URL = process.env.PYTHON_API_URL || 'http://194.242.56.38:8888/'; // --- 187.127.141.77 ---
+const PYTHON_API_URL = process.env.PYTHON_API_URL || 'http://187.127.141.77:8888/'; // --- 187.127.141.77 ---
 
 const OWNER_API_KEY = process.env.OWNER_API_KEY || 'oor_4a21a86defdca8eccbf4b5f63912d564';
 
